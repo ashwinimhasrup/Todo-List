46eb898725e574fa560ba6cc4baccd1198f85dc8
apoorva-kshirsagar-cognologix <apoorva.kshirsagar@cognologix.com>
2025-08-19 14:45:52 +0530

    update docker-compose for mongo connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 // require mongoose
 const mongoose = require('mongoose');
 // connect to database
-mongoose.connect(process.env.mongoDbUrl);
+mongoose.connect(process.env.mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // 5s instead of default 30s
+  });
 
 // acquire the connection (to check if it is successful)
 const db = mongoose.connection;
